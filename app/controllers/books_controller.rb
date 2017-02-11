1e3f972523827bad2ffaec37b1a15cda7da66bd5
JurgenHahn <jurgenehahn@gmail.com>
2017-02-11 14:03:19 -0500

    supports csv requests

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,17 @@
 class BooksController < ApplicationController
 
-def index
-  @books = Book.all
+  def index
+    @books = Book.all
 
-  respond_to do |format|
-    format.html
-    format.text
+    respond_to do |format|
+      format.html
+      format.text
+      format.csv do
+        csv_format = Book.generate_csv(@books)
+        render plain: csv_format
+      end
+    end
+
   end
-
-end
 
 end
